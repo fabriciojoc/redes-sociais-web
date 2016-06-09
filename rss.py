d78fea1f17084552909976ea361bbf71d3dbb97d
Fabricio Jose De Oliveira Ceschin <fjoceschin@inf.ufpr.br>
2016-06-09 20:57:50 -0300

    Remove comments

--- rss.py
+++ rss.py
@@ -5,9 +5,7 @@ import feedparser
 FEED_URL = "http://g1.globo.com/dynamo/rss2.xml"
 
 fp = feedparser.parse(FEED_URL)
-# print fp['feed']
 for f in fp.entries:
-    # print f.summary_detal.value
     print "Título: ", f.title
     print "Publicado: ", f.published
     print "Resumo:", f.summary
@@ -15,5 +13,4 @@ for f in fp.entries:
     for t in f.tags:
         tags += t.term + " "
     print "Tags: ", tags
-    # print f.value
     print '------'
