e06fc5509320997e53737be5665c7c4ffba47aa5
Fabricio Jose De Oliveira Ceschin <fjoceschin@inf.ufpr.br>
2016-06-10 11:32:53 -0300

    Add tokenization

--- nlp.py
+++ nlp.py
@@ -5,9 +5,17 @@ import nltk
 # Primeira execução: baixa os dados utilizados pela biblioteca
 # nltk.download()
 
+# EOS Detection
+
 txt = "Mr. Green killed Colonel Mustard in the study with the \
        candlestick. Mr. Green is not a very nice fellow."
 
 sentences = nltk.tokenize.sent_tokenize(txt)
 
-print sentences
+# Tokenization
+
+tokens = []
+for s in sentences:
+    tokens.append(nltk.tokenize.word_tokenize(s))
+
+print tokens
