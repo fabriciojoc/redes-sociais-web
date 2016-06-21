f44681ffc93ba85add8aeacc55eb8946b03b68a2
Fabricio Jose De Oliveira Ceschin <fjoceschin@inf.ufpr.br>
2016-06-21 12:17:21 -0300

    Add one more url to example 1

--- 1_boilerpipe_lib.py
+++ 1_boilerpipe_lib.py
@@ -3,6 +3,7 @@
 from boilerpipe.extract import Extractor
 
 URL='http://sportv.globo.com/site/eventos/mundial-de-motovelocidade/noticia/2016/06/em-duelo-eletrizante-rossi-vence-marquez-salom-e-homenageado.html'
+# URL='http://grandepremio.uol.com.br/motogp/noticias/rossi-supera-largada-ruim-vence-duelo-com-marquez-e-chega-a-10-vitoria-na-catalunha-lorenzo-abandona'
 
 extractor = Extractor(extractor='ArticleExtractor', url=URL)
 
