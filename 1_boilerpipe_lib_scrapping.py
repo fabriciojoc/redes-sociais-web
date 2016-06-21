864eedf5553981f02cc9d4f41b182af223513907
Fabrício Ceschin <fabricioceschin@gmail.com>
2016-06-21 13:50:05 -0300

    Add a limit to recommended news.

--- 1_boilerpipe_lib_scrapping.py
+++ 1_boilerpipe_lib_scrapping.py
@@ -4,11 +4,12 @@ from boilerpipe.extract import Extractor
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
 
+N=5
 URL='http://sportv.globo.com/site/eventos/mundial-de-motovelocidade/noticia/2016/06/em-duelo-eletrizante-rossi-vence-marquez-salom-e-homenageado.html'
 
 soup = BeautifulSoup(urlopen(URL).read(), "lxml")
 lista = soup.find_all("li", "noticia-plantao")
-for li in lista:
+for li in lista[:N]:
     url = li.find("a")
     extractor = Extractor(extractor='ArticleExtractor', url=url["href"])
     print extractor.getText().encode('utf-8')
