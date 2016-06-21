a216d01adaae04289443343e790cfa3317863e6e
Fabricio Jose De Oliveira Ceschin <fjoceschin@inf.ufpr.br>
2016-06-21 12:57:58 -0300

    Add web scrapping example to boilerpipe lib

---
+++ 1_boilerpipe_lib_scrapping.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+
+from boilerpipe.extract import Extractor
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+
+URL='http://sportv.globo.com/site/eventos/mundial-de-motovelocidade/noticia/2016/06/em-duelo-eletrizante-rossi-vence-marquez-salom-e-homenageado.html'
+
+soup = BeautifulSoup(urlopen(URL).read(), "lxml")
+lista = soup.find_all("li", "noticia-plantao")
+for li in lista:
+    url = li.find("a")
+    extractor = Extractor(extractor='ArticleExtractor', url=url["href"])
+    print extractor.getText().encode('utf-8')
+    print '-------------'
+
+extractor = Extractor(extractor='ArticleExtractor', url=URL)
+print extractor.getText().encode('utf-8')
